fcd321163d2b5b632b7727686177fdf84993632b
Mihret111 <mihret.kochito16@gmail.com>
2025-11-28 12:10:20 +0100

    Add prompt to retrieve user query

--- src/ui_node.cpp
+++ src/ui_node.cpp
@@ -6,6 +6,7 @@
 class UiNode : public rclcpp::Node     // inheriting from Node makes it a node
 {          // just need to have 2 publishers and no subscribed info is needed
 public:    
+    // def the constructor
     UiNode()                      // Const<- is public because we need to access it from outside                         
     : rclcpp::Node("ui_node"),    // name of the node = ui_node
     command_duration_(1.0)        // For how long
@@ -17,4 +18,42 @@ public:
             "/turtle2/cmd_vel", 10);
     }
 
+    // query user in a loop and fire the commander
+    void run()    // returns nth just fires the fires the commander
+    {
+        char chosen_turtle// prep a variable to hold the user input
+        while (rclcpp::ok())
+        {
+            // First user input
+            std::cout<< "Which turtle do you want ot move?\n"
+            << "Enter 1 or 2 ";      // ** later distinguish from the two after spawning and visualizing
+            std::cin>> chosen_turtle;    // checks is there is an error with the standard input stream
+
+            if (!std::cin.good()) {
+                std::cout << "Input error. Exiting application.\n";
+            break;
+            }
+
+            if (turtle_choice != '1' && turtle_choice != '2') {
+                std::cout << "Invalid choice. Please, enter 1 or 2.\n";
+            continue;
+            }
+
+            //Second user input
+            std::cout << "Enter the desired linear velocity: ";
+            std::cin >> velocity;
+
+            if (!std::cin.good()) {
+                std::cout << "Invalid input. Exiting application.\n";
+            break;
+            }
+
+        }
+    }
+
+
+
+
 }
+
+
